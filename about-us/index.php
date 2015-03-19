d6ab6d501f8a28a24a3f2da4a61c288820212a77
Pete Davies <pete.davies@mba.co.uk>
2015-03-19 20:49:55 +0000

    updates

--- about-us/index.php
+++ about-us/index.php
@@ -14,7 +14,7 @@ $pagename = "About Us";
                         <?php include '../includes/breadcrumb.php';?>
                         
                         <img class="headerImage" src="/assets/images/aboutus.jpg"/>
-                        <h2 class="bigText">About Us </h2> 
+                        <h2 class="bigText">About Us.</h2> 
                         <p>
                             Holmes Chapel Community Pre-school moved to our own premises in 1998 and are conveniently situated within the grounds of Holmes Chapel Primary School. We have places for 24 children aged from 2&#189; to school age. We are a committee-led registered charity (No. 1017112) and full members of the Pre-school Learning Alliance (PLA).
                         </p>
